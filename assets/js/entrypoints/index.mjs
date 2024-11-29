749fc4278fc447982174d64dba71194efb2b24e6
Eugene Kosukhin <kosuha606@gmail.com>
2024-11-29 22:41:09 +0300

    tailwind production and mobile fixes

--- assets/js/entrypoints/index.mjs
+++ assets/js/entrypoints/index.mjs
@@ -8,6 +8,9 @@ import { EntryPointRouting } from "../routing.mjs";
 import {
   Source,
 } from "patron-oop";
+import { StyleFetched } from "../lib/StyleFetched.mjs";
+
+new StyleFetched('https://raw.githubusercontent.com/kosukhin/patorn-design-system/refs/heads/main/dist/assets/index.css').install();
 
 const routing = new EntryPointRouting(
   ".loader",
